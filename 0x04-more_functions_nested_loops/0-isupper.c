7b683f39f41c5cb000ac0aefdc0dffcb79757608
Evanamaka <evanamakaokeke@gmail.com>
2023-02-23 12:58:16 -0800

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,15 @@
 
 /**
  * main - function that checks for uppercase character
- *
+ * @c: function parameter
  * Returns 1 if c is uppercase or Returns 0 otherwise
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
-	else 
+	}
 	return (0);
 }
